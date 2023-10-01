65bc1d672596273ffcaa8974f8b1272f0aa87ffe
Gaurang Bhardwaj <gaurang.bhardwaj@sleek.com>
2023-10-02 02:29:37 +0530

    bg video

--- src/components/main-screen/index.js
+++ src/components/main-screen/index.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import BigSurBackgroundImage from "images/big-sur-wallpaper.jpg";
+import ForestVideo from "videos/forest.mp4";
+import RocketLaunch from "videos/rocket-launch.mp4";
 import MenuBarController from "./menu-bar";
 import DesktopController from "./desktop";
 import DockController from "./dock";
@@ -12,11 +14,22 @@ const MainContainer = styled.div`
   padding: 0;
   width: 100%;
   height: 100vh;
-  background-image: url(${BigSurBackgroundImage});
-  background-size: cover;
+  /* background-image: url(${BigSurBackgroundImage});
+  background-size: cover; */
+`;
+
+const BackgroundVideoContainer = styled.div`
+  z-index: -1;
+  position: absolute;
+  top: 0;
+  left: 0;
+  max-width: 100vw;
+  max-height: 100vh;
+  overflow: hidden;
 `;
 
 const MenuBarContainer = styled.div`
+  z-index: 1;
   display: flex;
   flex-direction: row;
   width: 100%;
@@ -24,6 +37,7 @@ const MenuBarContainer = styled.div`
 `;
 
 const DesktopContainer = styled.div`
+  z-index: 1;
   display: flex;
   flex-direction: row;
   align-items: flex-start;
@@ -32,6 +46,7 @@ const DesktopContainer = styled.div`
 `;
 
 const DockContainer = styled.div`
+  z-index: 1;
   display: flex;
   flex-direction: row;
   width: 100%;
@@ -40,9 +55,43 @@ const DockContainer = styled.div`
   justify-content: center;
 `;
 
+const BG_VIDEOS = [ForestVideo, RocketLaunch];
+
 const MainScreenController = () => {
+  useEffect(() => {
+    setTimeout(async () => {
+      const video = document.getElementById("bg-video");
+      const videoSpeeds = [1, 0.75, 0.5];
+      try {
+        for (let i = 1; i <= 3; i++) {
+          setTimeout(() => {
+            if (video?.playbackRate) {
+              video.playbackRate = videoSpeeds[i];
+            }
+          }, i * 500);
+        }
+        setTimeout(() => video?.pause(), 2500);
+      } catch (err) {
+        console.log("UNABLE TO SET BG VIDEO");
+      }
+    }, 10000);
+  }, []);
+
   return (
     <MainContainer>
+      <BackgroundVideoContainer>
+        <video
+          id="bg-video"
+          playsInline
+          autoPlay
+          muted
+          loop
+          disablePictureInPicture
+          controlsList="nodownload"
+        >
+          <source src={BG_VIDEOS[0]} type="video/mp4" />
+        </video>
+      </BackgroundVideoContainer>
       <MenuBarContainer>
         <MenuBarController />
       </MenuBarContainer>
